1c82bc6aba81d992dace788beb89b343aa41c26e
Johan Buenaventura <buenaventurafranzjohanpoul@gmail.com>
2021-07-03 11:17:05 +0800

    defined table for users

--- db/migrate/20210629130351_create_users.rb
+++ db/migrate/20210629130351_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.text :username
-      t.text :password
+      t.integer :age
       
       t.timestamps
     end
