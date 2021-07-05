081735ad3c8961aaeb834912f4a338670a9139d0
Johan Buenaventura <buenaventurafranzjohanpoul@gmail.com>
2021-07-05 22:47:19 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "users#index"
-  get "/users" => "users#index"
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
+  get '/users/all' => 'users#all', as: 'all_subscribers'
   resources :users
+  
   # get '/users' => 'users#index', as: 'users_home'
   # get '/users/new' => 'users#new', as: 'users_new'
   # get '/users/edit' => 'users#new', as: 'users_edit'
