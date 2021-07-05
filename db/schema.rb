224a1c5b153c6acde373a3f9dadf779f563e5a85
Johan Buenaventura <buenaventurafranzjohanpoul@gmail.com>
2021-07-05 22:47:28 +0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,9 @@ ActiveRecord::Schema.define(version: 2021_06_29_130351) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.text "username"
-    t.integer "age"
+    t.string "firstname"
+    t.string "lastname"
+    t.text "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
