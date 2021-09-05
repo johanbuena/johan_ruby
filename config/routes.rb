d5a679f08ba56a58727f5e0187c75a4799ac85a6
Johan Buenaventura <buenaventurafranzjohanpoul@gmail.com>
2021-09-05 17:48:31 +0800

    Added root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
+  root "users#index"
   get '/users/all' => 'users#all', as: 'all_subscribers'
   resources :users
   
